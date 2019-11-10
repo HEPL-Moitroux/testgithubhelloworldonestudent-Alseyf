57821f48b841e80aac78f0f519c1b24976ff1a43
BREMER Denys <56680072+Alseyf@users.noreply.github.com>
2019-11-10 14:53:03 +0100

    Modif Denys

---
+++ DotNetLabo2BREMER-FRANCE/DotNetLabo2BREMER-FRANCE/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetLabo2BREMER_FRANCE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Labo 2 Modif Denys");
+
+            while (Console.Read() == 0) ;
+        }
+    }
+}
