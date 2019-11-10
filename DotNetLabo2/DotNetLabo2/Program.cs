39f669961e0616cdafddc30dca23172ac5cf526c
BREMER Denys <56680072+Alseyf@users.noreply.github.com>
2019-11-10 14:32:12 +0100

    Modif. Denys

---
+++ DotNetLabo2/DotNetLabo2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetLabo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Modification de Denys");
+            while(Console.Read()==0);
+        }
+    }
+}
